fc895f4cc6fac654315e1f5f93e22ee97386dd17
cryswang <cryswang@terpmail.umd.edu>
2020-06-22 19:41:33 -0400

    Added css to make front page more aesthetic and redesigned post_list.

--- blog/views.py
+++ blog/views.py
@@ -38,3 +38,6 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def bio_page(request):
+    return render(request, 'blog/bio_page.html')
